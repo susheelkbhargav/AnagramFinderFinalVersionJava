ccb31f4621f8cf4066732f0f12b785a6276b42a7
susheelkbhargav <susheelkbhargav@gmail.com>
2018-03-25 23:11:37 -0700

    final version removing the packages...can run from command line

---
+++ AnagramFinder.java
@@ -0,0 +1,133 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class AnagramFinder {
+    
+    public static void main(String[] args) throws FileNotFoundException,IOException {
+    	welcomeMessage();
+    	List <String> wordList= new ArrayList<String>();
+    	wordList=loadFile(args[0]);
+    	takeInput(wordList);
+        
+    }
+    
+    private static void takeInput(List<String>wordList) {
+    	while (true) {
+        	System.out.println();
+        	System.out.println();
+            System.out.print("AnagramFinder> ");
+            Scanner scanner2= new Scanner(System.in);
+        	String inputWord = scanner2.nextLine();
+        	 if ("exit".equals(inputWord)) {
+        		scanner2.close();
+             	System.exit(0); 
+             }
+        	 else {
+                 FindAnagramUsingCharacterCount(inputWord,wordList);
+
+        	 } 
+           
+        }
+    }
+    
+    private static ArrayList<String> loadFile(String args) throws FileNotFoundException {
+    	long startTime= System.currentTimeMillis();
+    	Scanner scanner = new Scanner(new File(args));
+    	ArrayList<String> wordList = new ArrayList<String>();
+    	while((scanner.hasNextLine())){
+            wordList.add(scanner.nextLine());
+            }
+        
+        scanner.close();
+        long stopTime= System.currentTimeMillis();
+
+        long endTime= stopTime-startTime;
+        System.out.println("Dictionary Loaded in "+ endTime+"" +"milli seconds");
+        
+       return wordList;
+    }
+    
+    private static void welcomeMessage() {
+    	System.out.println("Welcome To Anagram Finder");
+    	System.out.println();
+    	System.out.println("-----------------------------------------------------------------");
+    	
+    }
+    
+    private static void printDiscovery(List<String> wordList, String inputWord, long time) {
+    	if(wordList.isEmpty()|| wordList.size()==1) {
+    		System.out.println("No Anagram found for "+inputWord+" in "+ time + " ms");
+    	}
+    	else {
+    		System.out.println("Found "+ wordList.size()+ " anagrams in "+ time+ " ms");
+    		for(int i=0;i<wordList.size();i++) {
+    			
+    			if(i==wordList.size()-1) {
+    				System.out.print(wordList.get(i));
+    				System.out.println();
+    			}
+    			else {
+    				System.out.print(wordList.get(i)+ ", ");
+    			}
+    			
+    		}
+    	}
+    }
+    
+   
+   
+
+    private static void FindAnagramUsingCharacterCount(String inputWord, List<String> list) {
+ 
+        System.out.println("Using character count");
+        long startTime= System.currentTimeMillis();
+        List<String> anagramList= new ArrayList<String>();
+        for(int i=0; i<list.size();i++){
+           
+        	String word= list.get(i);
+           
+            if(isAnagramUsingCharacterCount(word,inputWord)){
+                
+            	anagramList.add(word);
+
+            }
+        }
+        long stopTime= System.currentTimeMillis();
+
+        long endTime= stopTime-startTime;
+        
+        printDiscovery(anagramList,inputWord,endTime);
+        
+    }
+
+
+    private static boolean isAnagramUsingCharacterCount(String s1, String s2){
+    	if(s1.equals(s2))
+    		return false;
+        if(s1.length()!=s2.length()){
+            return false;
+        }
+        int[] letters= new int[128];
+        String modifiedS1 = s1.replaceAll("\\s", "").toLowerCase();
+        String modifiedS2 = s2.replaceAll("\\s", "").toLowerCase();
+       // char[] arrayS2 = s2.replaceAll("\\s", "").toLowerCase().toCharArray();
+
+        for (char c:modifiedS1.toCharArray()){
+            //count each number of char in s
+            letters[c]++;
+        }
+        for(int i=0;i<modifiedS2.length();i++){
+            int c= (int)modifiedS2.charAt(i);
+            letters[c]--;
+            if(letters[c]<0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+   
+}
---
+++ readme.txt
@@ -0,0 +1,49 @@
+Anagram Finder
+
+This assignment finds all the anagrams of the given input in a dictionary text.  
+
+Getting Started
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
+
+Prerequisites
+What things you need to install the software and how to install them
+
+Give examples
+Installing
+A step by step series of examples that tell you have to get a development env running
+
+Say what the step will be
+
+Give the example
+
+
+until finished
+End with an example of getting some data out of the system or using it for a little demo
+
+Running the tests
+Compile with --> javac AnagramFinder.java
+Run with --> java AnagramFinder dictionary.txt
+
+
+Break down into end to end tests
+Explain what these tests test and why
+
+Give an example
+And coding style tests
+Explain what these tests test and why
+
+Give an example
+Deployment
+Add additional notes about how to deploy this on a live system
+
+Built With
+IntelliJ
+
+
+Versioning
+We use SemVer for versioning. For the versions available, see the tags on this repository.
+
+Authors
+Susheel Kumar Bhargav Kaparaboyna
+
+
